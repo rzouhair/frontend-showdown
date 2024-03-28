bab257f69d770efe2db94705282a8214703a8739
Zouhair <rouikazouhair@gmail.com>
2024-03-28 13:49:00 +0000

    Created the Hero section - Pricing and Stats section

---
+++ components/PricingSection.vue
@@ -0,0 +1,67 @@
+<script setup lang="ts">
+import type { PackageProps } from '~/utils/@types';
+
+const packages: PackageProps[] = [
+  {
+    id: 1,
+    title: 'Basic',
+    icon: '/Leaf.png',
+    pricePerSeat: 50,
+    features: [
+      { id: 1, text: 'All Video Courses ', included: true },
+      { id: 2, text: 'Vue.Js Master class', included: true },
+      { id: 3, text: 'Developer assist Slack channel', included: true },
+      { id: 4, text: 'Live Weekly QnA', included: false },
+      { id: 5, text: '1 x ws ticket per license', included: false },
+    ],
+    link: 'https://www.vueschool.io/'
+  },
+  {
+    id: 2,
+    title: 'Professional',
+    icon: '/Rocket.png',
+    pricePerSeat: 50,
+    features: [
+      { id: 1, text: 'All Video Courses ', included: true },
+      { id: 2, text: 'Vue.Js Master class', included: true },
+      { id: 3, text: 'Developer assist Slack channel', included: true },
+      { id: 4, text: 'Live Weekly QnA', included: true, highlighted: true },
+      { id: 5, text: '1 x ws ticket per license', included: true, highlighted: true },
+    ],
+    suggestedCourse: {
+      thumbnail: '/Masterclass.png',
+      title: 'The Vue 3 Masterclass'
+    },
+    link: 'https://www.vueschool.io/'
+  },
+  {
+    id: 3,
+    title: 'Basic',
+    icon: '/Leaf.png',
+    pricePerSeat: 50,
+    features: [
+      { id: 1, text: 'All Video Courses ', included: true },
+      { id: 2, text: 'Vue.Js Master class', included: true },
+      { id: 3, text: 'Developer assist Slack channel', included: true },
+      { id: 4, text: 'Live Weekly QnA', included: true },
+      { id: 5, text: '4 x Virtual Workshop of your choice', included: true, highlighted: true },
+    ],
+    link: 'https://www.vueschool.io/'
+  },
+]
+</script>
+
+<template>
+  <section class="w-container relative z-10">
+    <h2 class="heading-1 max-w-screen-md text-center self-center w-full mx-auto md:mb-30 mb-14">
+      Discounted <span class="text-gradient-accent">Corporate Training</span> Bundles
+    </h2>
+    <div class="grid grid-cols-1 gap-6 min-[1230px]:grid-cols-3 min-[1230px]:gap-7.5">
+      <pricing-card
+        v-for="plan in packages"
+        :key="plan.id"
+        :package="plan"
+      />
+    </div>
+  </section>
+</template>
---
+++ components/StatsSection.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+const stats = reactive([
+  { id: 1, count: 763, label: 'Video lessons', icon: '/Play.png' },
+  { id: 2, count: 40, label: 'Courses', icon: '/Clip.png' },
+  { id: 3, count: 64, label: '15 Hours', icon: '/Clock.png' },
+])
+</script>
+
+<template>
+  <section class="w-container flex flex-col gap-8 flex-wrap tight:flex-row tight:items-center">
+    <div class="text-center tight:text-left tight:w-1/2 tight:max-w-max max-w-[35.625rem] self-center">
+      <p class="heading-3 text-gradient-accent mb-3">YOUR #1 SOURCE OF VUE.JS COURSES</p>
+      <h2 class="heading-1">Full Stack <span class="text-gradient-accent">Vue Training</span> Solutions</h2>
+    </div>
+    <div class="flex items-start justify-center sm:gap-[3.75rem] gap-6">
+      <stats-item
+        v-for="item in stats"
+        :key="item.id"
+        v-bind="item"
+      />
+    </div>
+  </section>
+</template>
